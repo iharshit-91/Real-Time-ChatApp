7af684146ccfab680f01b5eb1ed2cf5f26398055
harshit Maurya <iharshit9143@gmail.com>
2026-03-14 02:43:21 -0700

    Update cookie settings for token in auth controller

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -47,8 +47,8 @@ export const signUp = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       maxAge: 100 * 24 * 60 * 60 * 1000,
-      sameSite: "Strict",
-      secure: false,
+      sameSite: "None",
+      secure: true,
     });
 
     return res.status(201).json(user);
@@ -83,8 +83,8 @@ export const login = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       maxAge: 100 * 24 * 60 * 60 * 1000,
-      sameSite: "Strict",
-      secure: false,
+      sameSite: "None",
+      secure: true,
     });
 
     return res.status(200).json(user);
